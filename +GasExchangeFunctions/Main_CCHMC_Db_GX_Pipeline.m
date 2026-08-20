2997eea5fe16a1f2cd74ef28a9e319162e428516
Abdullah Bdaiwi <bdaiwias@mail.uc.edu>
2026-08-20 17:11:00 -0400

    Update Main_CCHMC_Db_GX_Pipeline.m

--- +GasExchangeFunctions/Main_CCHMC_Db_GX_Pipeline.m
+++ +GasExchangeFunctions/Main_CCHMC_Db_GX_Pipeline.m
@@ -164,6 +164,7 @@ for i = 125 %2:nSubjects%:nSubjects % always start from 2
         elseif RuneCol{i} == 1
             MainInput.UpdatedNote = T{i, 13};
             MainInput.UpdatedImageQuality  = T{i, 15};
+            MainInput.ProcessingNotes = MainInput.UpdatedNote;
             MainInput.ImageQuality = MainInput.UpdatedImageQuality;
             GasExchangeFunctions.CCHMC_Db_GX_Pipeline_rerun(MainInput,analysispath);
         end
